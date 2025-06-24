b1acb16137eff35905f8b1b624359d26e9d8aab3
cafali <77307171+cafali@users.noreply.github.com>
2025-06-24 16:43:30 +0200

    cafali.art

--- scripts/global.js
+++ scripts/global.js
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
+// google analytics
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
