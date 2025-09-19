b152a7952fdd950c1c6426dd729bdcf84a74567e
cafali <77307171+cafali@users.noreply.github.com>
2025-09-19 15:47:23 +0200

    cafali.art

--- scripts/global.js
+++ scripts/global.js
@@ -42,3 +42,11 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('scroll', highlightCurrentSection);
   highlightCurrentSection();
 });
+
+
+// gooogle analytics
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VLDRNTTKV9');
